24d26797cfbda0917daa6014eee8a87f9d058c33
chokchai9900 <48384299+chokchai9900@users.noreply.github.com>
2019-08-15 11:27:13 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 //using WebManageAPI.Function;
 using MongoDB.Driver;
-using MongoDB.Bson;
 using NSOWater.HotMigration.HotModels;
-using Microsoft.WindowsAzure.Storage;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
 
 namespace NsoGetData
 {
@@ -13,6 +16,7 @@ namespace NsoGetData
         static async Task Main(string[] args)
         {
             int count = 0;
+            string data = "10";
             //Nso Real
             //var client = new MongoClient("mongodb://dbagent:Nso4Passw0rd5@mongodbproykgte5e7lvm7y-vm0.southeastasia.cloudapp.azure.com/nso");
             //var database = client.GetDatabase("nso");
@@ -24,6 +28,8 @@ namespace NsoGetData
             var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=nsosurvey;AccountKey=O8rkX9HlkBC6VDh5XbMFcec3ndofVuh21yCkLdODZXX6d4FeWJ0H20SeFtxqDw9Ii66mWCxRsYLJkVYVsLrckw==;EndpointSuffix=core.windows.net");
             var blobClient = storageAccount.CreateCloudBlobClient();
             var container = blobClient.GetContainerReference("surveys");
+            
+            BlobContinuationToken blobContinuationToken = null;
 
             Console.WriteLine("database : " + database);
             Console.WriteLine("collection : " + collection);
@@ -31,19 +37,25 @@ namespace NsoGetData
             Console.WriteLine("Please Wait........");
 
             var uploadLogs = collection.Aggregate()
-                .Match(x => x.Province == "10")
+                .Match(x => x.Province == data)
                 .ToList();
-
+            string path = @"d:\\demotext.txt";
             //var uploadLogs = await collection.Find(x => x.UserId == "10506941").ToListAsync();
             Console.WriteLine("Total data = " + uploadLogs.Count);
             string[] a = new string[uploadLogs.Count];
             foreach (var log in uploadLogs)
             {
                 count++;
-                Console.Write(count + " : ");
+                Console.Write(count + " : " + log.CreationDateTime);
                 Console.Write(log.BlobName);
                 Console.Write("  ");
                 Console.WriteLine(log.ContainerName);
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.Write(log.CreationDateTime + "  ");
+                    writer.Write(log.BlobName + " ");
+                    writer.WriteLine(log.ContainerName);
+                }
             }
             Console.WriteLine("Total data = " + uploadLogs.Count);
             Console.Read();
